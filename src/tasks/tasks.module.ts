6c243843473d8332f975efc186122be4ad89c5a9
Drew <glavich_drew@yahoo.com>
2022-01-14 23:42:40 +0900

    feat: protect tasks routes with an auth guard and created a user decorator

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TasksRepository]), AuthModule],
   controllers: [TasksController],
   providers: [TasksService],
-  imports: [TypeOrmModule.forFeature([TasksRepository])],
 })
 export class TasksModule {}
